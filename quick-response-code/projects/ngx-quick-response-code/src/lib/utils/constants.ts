14101da563fed9768eaf8da504a179be29fe07c7
Pawan Mishra <pawanmsr@outlook.com>
2025-10-29 17:53:06 +0530

    update contants

--- quick-response-code/projects/ngx-quick-response-code/src/lib/utils/constants.ts
+++ quick-response-code/projects/ngx-quick-response-code/src/lib/utils/constants.ts
@@ -1,5 +1,4 @@
 const NUMERIC_REGULAR_EXPRESSION = /^\d*$/;
 const ALPHANUMERIC_REGULAR_EXPRESSION = /^[\dA-Z $%*+-./:]*$/;
 const BYTE_REGULAR_EXPRESSION = /^[\x00-\xff]*$/;
-
 const POSITION_MARKER_SIZE: number = 7;
