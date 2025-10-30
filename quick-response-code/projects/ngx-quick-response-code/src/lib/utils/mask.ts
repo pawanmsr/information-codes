c0d758f75cc254a8bba8fe028cec3d3b0fd7b655
Pawan Mishra <pawanmsr@outlook.com>
2025-10-30 15:12:00 +0530

    add mask conditions

---
+++ quick-response-code/projects/ngx-quick-response-code/src/lib/utils/mask.ts
@@ -0,0 +1,37 @@
+class Mask {
+    constructor(private pattern: number) {}
+
+    public condition(pattern: number, row: number, column: number): boolean {
+        switch (pattern) {
+            case 0:
+                return (row + column) % 2 === 0;
+                break;
+            case 1:
+                return row % 2 === 0;
+                break;
+            case 2:
+                return column % 3 === 0;
+                break;
+            case 3:
+                return (row + column) % 3 === 0;
+                break;
+            case 4:
+                return (row / 2 + column / 3) % 2 === 0;
+                break;
+            case 5:
+                return (row + column) % 2 + (row * column) % 3 === 0;
+                break;
+            case 6:
+                return ((row * column) % 3 + row * column) % 2 === 0;
+                break;
+            case 7:
+                return ((row * column) % 3 + row + column) % 2 === 0;
+                break;
+        
+            default:
+                break;
+        }
+
+        return false;
+    }
+}
