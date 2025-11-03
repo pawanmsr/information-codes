889d2cce717f9530ba31049c5491bc48d4c3cb6b
Pawan Mishra <pawanmsr@outlook.com>
2025-11-03 15:41:37 +0530

    combine alignment and finder

--- quick-response-code/projects/ngx-quick-response-code/src/lib/utils/position.ts
+++ quick-response-code/projects/ngx-quick-response-code/src/lib/utils/position.ts
@@ -11,16 +11,36 @@ export class Markers {
             x: this.size(version) - POSITION_MARKER_SIZE,
             y: 0
         }
+
+        if (version > 1) {
+            this.generate();
+        }
     }
 
     private size(version: number): number {
         return version * 4 + 17;
     }
 
-    private gaps(version: number): number {
+    private distance(version: number): number {
         return this.size(version) - 6 - 7;
     }
 
+    private generate(): void {
+        let distance = this.distance(this.version);
+        let intervals = Math.floor(this.version / 7 + 1);
+        let step = Math.round(distance / intervals);
+
+        step += step % 2;
+
+        this.positions.push(6);
+        for (let i = 0; i < intervals; i++) {
+            this.positions.push(6 + distance - step * (intervals - i - 1));
+        }
+    }
+
+    private positions: number[] = [];
+
+    // TODO: make private?
     public topRight: Coordinate;
     public bottomLeft: Coordinate;
     public topLeft: Coordinate = {
@@ -28,7 +48,27 @@ export class Markers {
         y: 0
     };
 
-    public asArray(): Coordinate[] {
+    public finderPatterns(): Coordinate[] {
         return [this.topLeft, this.topRight, this.bottomLeft];
+    }
+
+    public alignmentPatterns(): Coordinate[] {
+        let n: number = this.positions.length;
+
+        let coordinates: Coordinate[] = [];
+        for (let i = 0; i < n; i++) {
+            for (let j = 0; j < n; j++) {
+                if (i == 0 && j == 0) continue;
+                if (i == 0 && j == n - 1) continue;
+                if (i == n - 1 && j == 0) continue;
+
+                coordinates.push({
+                    x: this.positions[i],
+                    y: this.positions[j]
+                });
+            }
+        }
+
+        return coordinates;
     }
 }
