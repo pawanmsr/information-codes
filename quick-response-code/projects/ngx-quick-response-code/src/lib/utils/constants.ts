80aa105d60fca1e732d144231da629b20677a078
Pawan H M <25368219+pawanmsr@users.noreply.github.com>
2025-11-03 15:43:34 +0530

    Alignment (#8)

--- quick-response-code/projects/ngx-quick-response-code/src/lib/utils/constants.ts
+++ quick-response-code/projects/ngx-quick-response-code/src/lib/utils/constants.ts
@@ -3,3 +3,4 @@ const ALPHANUMERIC_REGULAR_EXPRESSION = /^[\dA-Z $%*+-./:]*$/;
 const BYTE_REGULAR_EXPRESSION = /^[\x00-\xff]*$/;
 
 const POSITION_MARKER_SIZE: number = 7;
+const ALIGNMENT_PATTERN_SIZE: number = 5;
