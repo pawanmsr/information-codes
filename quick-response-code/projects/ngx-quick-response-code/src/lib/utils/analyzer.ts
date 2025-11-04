e1dd0df5ed71f42a236fa27c4062792ef25b4cf1
Pawan Mishra <pawanmsr@outlook.com>
2025-11-04 14:57:18 +0530

    add bit length

--- quick-response-code/projects/ngx-quick-response-code/src/lib/utils/analyzer.ts
+++ quick-response-code/projects/ngx-quick-response-code/src/lib/utils/analyzer.ts
@@ -24,6 +24,33 @@ class Analyzer {
 
         return 0b0111;
     }
+
+    public bitLength(): number {
+        switch (this.encoding) {
+            case 1:
+                return 10 * Math.floor(this.text.length / 3) + 
+                    (this.text.length % 3 ? (this.text.length % 3) * 3 + 1 : 0);
+                break;
+            
+            case 2:
+                return 11 * Math.floor(this.text.length / 2) + 
+                    6 * (this.text.length & 1)
+                break;
+            
+            case 3:
+                return this.text.length * 8;
+                break;
+            
+            case 4:
+                return this.text.length * 13;
+                break;
+        
+            default:
+                break;
+        }
+
+        return 0;
+    }
     
     public minimumVersion(minimumErrorLevel: number): number | undefined {
         let version: number = VERSION.MIN;
