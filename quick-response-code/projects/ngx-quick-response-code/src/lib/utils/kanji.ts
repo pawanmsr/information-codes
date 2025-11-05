4559969a90b0cd55fd5b5022d2d0775c6524628d
Pawan Mishra <pawanmsr@outlook.com>
2025-11-05 17:38:51 +0530

    decompress shift jis

--- quick-response-code/projects/ngx-quick-response-code/src/lib/utils/kanji.ts
+++ quick-response-code/projects/ngx-quick-response-code/src/lib/utils/kanji.ts
@@ -1,13 +1,25 @@
 import { KANJI_TABLE } from "./tables";
 
 export class KanjiEncoder {
-    constructor() {}
+    private kanjiTable: {[key: string]: number} = {}
+    
+    constructor() {
+        for (const key in KANJI_TABLE) {
+            let characters: string[] = key.split("");
+            let value: number = KANJI_TABLE[key];
+
+            for (const character of characters) {
+                this.kanjiTable[character] = value;
+                value += 1;
+            }
+        }
+    }
 
     public encode(text: string): Uint16Array<ArrayBuffer> {
         let data: Uint16Array<ArrayBuffer> = new Uint16Array(text.length);
         for (let i = 0; i < text.length; i++) {
-            if (text[i] in KANJI_TABLE) {
-                data[i] = KANJI_TABLE[text[i]];
+            if (text[i] in this.kanjiTable) {
+                data[i] = this.kanjiTable[text[i]];
                 continue;
             }
 
