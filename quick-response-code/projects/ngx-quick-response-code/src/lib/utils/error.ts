774fbbd43d7321369316c35ad45f3ca168431302
Pawan H M <25368219+pawanmsr@users.noreply.github.com>
2025-11-06 15:44:36 +0530

    Polynomial (#15)

--- quick-response-code/projects/ngx-quick-response-code/src/lib/utils/error.ts
+++ quick-response-code/projects/ngx-quick-response-code/src/lib/utils/error.ts
@@ -22,6 +22,15 @@ class ErrorCorrection {
     private arithmetic(x: number, y: number): number {
         return x ^ y;
     }
+    
+    private multiply(x: number, y: number): number {
+        let exponent: number = this.logs[x] + this.logs[y];
+        if (exponent >= FIELD_SIZE) {
+            exponent %= 255;
+        }
+
+        return this.powers[exponent];
+    }
 
     private polynomialDivision(divisor: Uint8Array, divident: Uint8Array): Uint8Array {
         let n: number = divisor.length;
@@ -36,10 +45,54 @@ class ErrorCorrection {
 
         return divident.slice(0, n - 1);
     }
+    
+    private polynomialProduct(x: Uint8Array, y: Uint8Array): Uint8Array {
+        let z: Uint8Array = new Uint8Array(x.length + y.length - 1);
+        for (let i = 0; i < x.length; i++) {
+            for (let j = 0; j < y.length; j++) {
+                z[i + j] = this.arithmetic(z[i + j],
+                    this.multiply(x[i], y[j]));
+            }
+        }
+
+        return z;
+    }
+
+    private generatePolynomial(n: number): Uint8Array {
+        let coefficients: Uint8Array = new Uint8Array([this.powers[0]]);
+        for (let d = 0; d < n; d++) {
+            // Coefficients are elements of Galois(256).
+            // Elements in Galois Field are positive integers.
+            let multiplier: Uint8Array = new Uint8Array([this.powers[0], this.powers[d]]);
+            coefficients = this.polynomialProduct(coefficients, multiplier);
+        }
+
+        return coefficients;
+    }
+
+    private messagePolynomial(data: Uint8Array, version: number): Uint8Array {
+        let length: number = data.length / BITS_IN_BYTE;
+        let padding: number = this.totalCodewords(version);
+        let coefficients: Uint8Array = new Uint8Array(length + padding);
+        for (let i = 0; i < length; i++) {
+            let value: number = 0;
+            let multiplier: number = 1;
+            for (let j = (i + 1) * BITS_IN_BYTE - 1; j >= i * BITS_IN_BYTE; j--) {
+                value += data[j] * multiplier;
+                multiplier <<= 1;
+            }
+
+            coefficients[length + padding - i] = value;
+        }
+
+        return coefficients;
+    }
 
     public totalCodewords(version: number): number {
         let index: number = (version - 1) * VERSION_MULTIPLIER + this.level;
-        return ERROR_CORRECTION_CODEWORDS_PER_BLOCK[index] * 
+        let count: number = ERROR_CORRECTION_CODEWORDS_PER_BLOCK[index] * 
             (BLOCK_COUNT.GROUP_ONE[index] + BLOCK_COUNT.GROUP_TWO[index]);
+
+        return count;
     }
 }
