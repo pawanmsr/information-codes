2f9019b5ebf9ca5538445009a3d4c84def112f27
Pawan Mishra <pawanmsr@outlook.com>
2026-03-07 14:17:16 +0530

    secondary signals

---
+++ morse-code/interface/src/routes/Tap.svelte
@@ -0,0 +1,19 @@
+<script>
+	import './page.css';
+	
+    let props = $props();
+</script>
+
+<button class="text-lg font-bold transform-gpu rounded-full
+	shadow-[0_4px_0_hsl(240,100%,30%)] text-blue-50 uppercase
+    text-center -translate-y-2 active:-translate-y-1 bg-blue-800
+	active:shadow-[0_1px_0_hsl(240,100%,30%)] cursor-grab" {...props}>
+    {props.value}
+</button>
+
+<style>
+	button {
+		width: 2em;
+		height: 2em;
+	}
+</style>
