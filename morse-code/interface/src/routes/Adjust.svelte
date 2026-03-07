1f180af4951c0bdc1927c96e8477920980ed25bb
Pawan Mishra <pawanmsr@outlook.com>
2026-03-07 14:16:41 +0530

    control for adjustment

---
+++ morse-code/interface/src/routes/Adjust.svelte
@@ -0,0 +1,15 @@
+<script>
+	import './page.css';
+	
+    let props = $props();
+</script>
+
+<button class="text-lg transform-gpu rounded-sm bg-amber-700 w-4
+	text-amber-50 uppercase text-center -translate-y-1
+    active:-translate-y-0 shadow-[0_4px_0_hsl(25,100%,30%)]
+	active:shadow-[0_1px_0_hsl(25,100%,30%)] cursor-grab" {...props}>
+    {props.value}
+</button>
+
+<style>
+</style>
